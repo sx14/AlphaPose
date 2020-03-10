5611d829992fec0fce143660eb517bc79b1309bc
sunx <1059363093@qq.com>
2020-03-10 11:41:33 +0800

    run on dataset

--- PoseFlow/matching.py
+++ PoseFlow/matching.py
@@ -31,15 +31,9 @@ def orb_matching(img1_path, img2_path, vidname, img1_id, img2_id):
     out_path = "%s/%s_%s_orb.txt"%(vidname, img1_id, img2_id)
     # print(out_path)
     
-    if isinstance(img1_path, str):
-        img1 = cv2.cvtColor(cv2.imread(img1_path), cv2.COLOR_BGR2RGB)
-    else:
-        img1 = cv2.cvtColor(img1_path, cv2.COLOR_BGR2RGB)
-    if isinstance(img2_path, str):
-        img2 = cv2.cvtColor(cv2.imread(img2_path), cv2.COLOR_BGR2RGB)
-    else:
-        img2 = cv2.cvtColor(img2_path, cv2.COLOR_BGR2RGB)
-    
+    img1 = cv2.cvtColor(cv2.imread(img1_path), cv2.COLOR_BGR2RGB)
+    img2 = cv2.cvtColor(cv2.imread(img2_path), cv2.COLOR_BGR2RGB)
+
     # Initiate ORB detector
     orb = cv2.ORB_create(nfeatures=10000, scoreType=cv2.ORB_FAST_SCORE)
 
--- scripts/inference.sh
+++ scripts/inference.sh
@@ -1,8 +1,13 @@
 set -x
 
-CONFIG=$1
-CKPT=$2
-VIDEO=$3
+# CONFIG=$1
+# CKPT=$2
+# VIDEO=$3
+
+CONFIG=configs/coco/resnet/256x192_res152_lr1e-3_1x-duc.yaml
+CKPT=pretrained_models/fast_421_res152_256x192.pth
+VIDEO=/media/sunx/Data/2793806282.mp4
+
 OUTDIR=${4:-"./examples/res"}
 
 python scripts/demo_inference.py \
